9c587516c91d56b779a65eeab1f0a594a6dfa66e
ShaminaTatiana <78913652+ShaminaTatiana@users.noreply.github.com>
2021-03-15 15:25:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,6 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
   int i = 0, j = size - 1, countNum = 0;
     while (i < j) {
         int mid = (i + j) / 2;
